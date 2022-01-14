772c2cddc8d6143f7414b950fd706da788a2b4eb
Thiago Thomazini <tt.thomazini@gmail.com>
2022-01-13 21:05:26 -0300

    codewars :)

---
+++ src/multiplo3E5.java
@@ -0,0 +1,28 @@
+
+/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+*/
+
+
+public class multiplo3E5 {
+	public int solution(int number) {
+		int res = 0;
+		int cont = 0;
+		while (cont < number) {
+			if (cont % 3 == 0 && cont % 5 == 0) {
+				res += cont;
+				cont++;
+			}
+			if (cont % 3 == 0) {
+				res += cont;
+			}
+			if (cont % 5 == 0) {
+				res += cont;
+			}cont++;
+		}
+		return res;
+	}
+}
